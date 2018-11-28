f31dfef03cfb15358f8ce462fedf3f17d4b233c5
Arlo Guthrie <aguthrie@instructure.com>
2018-11-28 12:21:12 -0600

    2018-11-27 and 28

--- sketches/2018-11-27.js
+++ sketches/2018-11-27.js
@@ -1,38 +1,18 @@
-let start = true;
-
-// Border Width
-const bw = 20;
-
 function setup() {
   const myCanvas = createCanvas(660, 840);
   myCanvas.parent('my-canvas');
-  background('white');
   strokeWeight(4);
   noLoop();
 }
 
 function draw() {
-  push();
-  strokeWeight(bw);
-  noFill();
-  rect(bw / 2, bw / 2, width - bw, height - bw);
-  pop();
+  background('white');
 
-  if (start) {
-    push();
-    fill(255);
-    text('Click canvas for more', 5, 15);
-    pop();
+  for (let i = 0; i < 1000; i++) {
+    triangle(
+      0, 0,
+      width, random(0, height),
+      random(0, width), height
+    );
   }
-
-  triangle(
-    bw, bw,
-    width - bw, random(bw, height - bw),
-    random(bw, width - bw), height - bw
-  );
-}
-
-function mouseClicked() {
-  start = false;
-  redraw(10);
 }
