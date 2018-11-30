c915bb6cdf9fba6e8d392e22361abcebadb8efe9
Arlo Guthrie <aguthrie@instructure.com>
2018-11-30 15:16:26 -0600

    All I really cared about was prettier anyway

--- scripts/serve.js
+++ scripts/serve.js
@@ -7,7 +7,10 @@ const chokidar = require('chokidar');
 const app = express();
 const port = 8080;
 
-const sketchFiles = fs.readdirSync('./sketches/').reverse().map(sketch => path.basename(sketch, '.js'));
+const sketchFiles = fs
+  .readdirSync('./sketches/')
+  .reverse()
+  .map(sketch => path.basename(sketch, '.js'));
 const template = fs.readFileSync('./page.hbs', 'utf8');
 const html = handlebars.compile(template);
 
@@ -16,7 +19,7 @@ app.use('/js', express.static('sketches'));
 function getContext(currentSketch) {
   return {
     sketches: sketchFiles.map(sketchFile => ({
-      sketch: sketchFile + '.js',
+      sketch: `${sketchFile}.js`,
       url: sketchFile,
       isCurrent: sketchFile === currentSketch
     })),
@@ -24,11 +27,14 @@ function getContext(currentSketch) {
     local: true
   };
 }
-app.get('/', (_, res) => res.send(html(getContext(sketchFiles[0]))));
-app.get('/sketch/:file', (req, res) => res.send(html(getContext(req.params.file))));
+app.get('/', (_, res) =>
+  res.send(html(getContext(sketchFiles[0]))));
+app.get('/sketch/:file', (req, res) =>
+  res.send(html(getContext(req.params.file))));
 
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
 io.on('connection', socket => {
   const watcher = chokidar.watch('./sketches/*', {
     ignored: /(^|[\/\\])\../,
@@ -37,4 +43,4 @@ io.on('connection', socket => {
   watcher.on('change', () => io.emit('update'));
 });
 
-http.listen(port, () => console.log(`👂 Listening at http://localhost:${port}/`));
+http.listen(port, () => console.log(`👂 Listening at http://localhost:${port}/`), );
