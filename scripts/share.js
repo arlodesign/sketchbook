57795599acd7a9ea2c6f8a4892dc781b5e653d1c
Arlo Guthrie <aguthrie@instructure.com>
2018-11-30 15:40:15 -0600

    RSS

--- scripts/share.js
+++ scripts/share.js
@@ -3,6 +3,7 @@ const path = require('path');
 const handlebars = require('handlebars');
 const rimraf = require('rimraf');
 const ghpages = require('gh-pages');
+const rss = require('rss');
 
 const sketchFiles = fs.readdirSync('./sketches/')
   .reverse()
@@ -29,11 +30,26 @@ fs.mkdirSync('./dist');
 fs.mkdirSync('./dist/sketch');
 fs.mkdirSync('./dist/js');
 
+const feed = new rss({
+  title: 'sketchbook.arlo.me',
+  description: 'Explorations of generative art using p5.js',
+  feed_url: "https://sketchbook.arlo.me/feed.rss",
+  site_url: "https://sketchbook.arlo.me/"
+});
+const feedLimit = 20;
+let feedItems = 0;;
+
 sketchFiles.forEach(sketch => {
   fs.writeFileSync(`./dist/sketch/${sketch}.html`, html(getContext(sketch)));
   fs.copyFileSync(`./sketches/${sketch}.js`, `./dist/js/${sketch}.js`);
+  ++feedItems <= feedLimit && feed.item({
+    title: sketch,
+    url: `https://sketchbook.arlo.me/sketch/${sketch}`,
+    date: sketch
+  });
 });
 fs.writeFileSync('./dist/index.html', html(getContext(sketchFiles[0])));
+fs.writeFileSync('./dist/feed.rss', feed.xml());
 
 ghpages.publish('./dist', error => {
   if (error) throw error;
