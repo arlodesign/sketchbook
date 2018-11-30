941eb0598d9bc1de07a49239071e07e56fbabb28
Arlo Guthrie <aguthrie@instructure.com>
2018-11-30 09:44:26 -0600

    Fix filename generation

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -6,36 +6,38 @@ function setup() {
   pixelDensity(4);
   const myCanvas = createCanvas(660, 840);
   myCanvas.parent('my-canvas');
-  background('white');
   // SETUP
 }
 
 function draw() {
+  background('white');
   // DRAW
 }`;
 
-const DateObj = new Date();
-let filename = [
-  DateObj.getFullYear(),
-  DateObj.getMonth() + 1,
-  DateObj.getDate()
-].join('-');
-
-if (fs.existsSync(`./sketches/${filename}.js`)) {
-  // use tomorrow's date
-  filename = [
-    DateObj.getFullYear(),
-    DateObj.getMonth() + 1,
-    DateObj.getDate() + 1
-  ].join('-');
+function leftPad(num) {
+  return num < 10 ? `0${num}` : num;
 }
 
-if (fs.existsSync(`./sketches/${filename}.js`)) {
+function filename(date) {
+  return [
+    date.getFullYear(),
+    leftPad(date.getMonth() + 1),
+    leftPad(date.getDate())
+  ].join('-')
+}
+
+const DateObj = new Date();
+
+if (fs.existsSync(`./sketches/${filename(DateObj)}.js`)) {
+  DateObj.setDate(DateObj.getDate() + 1);
+}
+
+if (fs.existsSync(`./sketches/${filename(DateObj)}.js`)) {
   // Wait until tomorrow.
   console.log('⚠️ Files already exist.');
 } else {
-  fs.writeFile(`./sketches/${filename}.js`, template, error => {
+  fs.writeFile(`./sketches/${filename(DateObj)}.js`, template, error => {
     if (error) throw error;
-    console.log(`🎉 ${filename}.js created.`);
+    console.log(`🎉 ${filename(DateObj)}.js created.`);
   })
 }
