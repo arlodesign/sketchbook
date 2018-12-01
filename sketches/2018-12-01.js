3fe46e1b284a99b8c4f6fbc8ea7f8ba304680fd8
Arlo Guthrie <aguthrie@instructure.com>
2018-12-01 14:45:28 -0600

    2018-12-01

---
+++ sketches/2018-12-01.js
@@ -0,0 +1,58 @@
+window.sketchHeadline = "Move pointer over sketch.";
+
+const centerWeight = 20;
+const centerWeightGrowth = 6;
+
+function setup() {
+  pixelDensity(4);
+  const myCanvas = createCanvas(660, 840);
+  myCanvas.parent('my-canvas');
+  strokeWeight(3);
+  fill(0);
+}
+
+function draw() {
+  background('white');
+  const centerWeightX = map(
+    Math.abs((width / 2) - mouseX),
+    0, width / 2,
+    centerWeight, centerWeight * centerWeightGrowth
+  );
+  const centerWeightY = map(
+    Math.abs((height / 2) - mouseY),
+    0, height / 2,
+    centerWeight * centerWeightGrowth, centerWeight
+  );
+  // Sides, starting top and going clockwise
+  stroke(0);
+  line(mouseX, 0, mouseX, mouseY);
+  line(width, mouseY, mouseX, mouseY);
+  line(mouseX, height, mouseX, mouseY);
+  line(0, mouseY, mouseX, mouseY);
+  // Corners, starting top left and going clockwise
+  stroke(255);
+  quad(
+    mouseX, mouseY,
+    mouseX - centerWeightX, mouseY,
+    0, 0,
+    mouseX, mouseY - centerWeightY
+  );
+  quad(
+    mouseX, mouseY,
+    mouseX + centerWeightX, mouseY,
+    width, 0,
+    mouseX, mouseY - centerWeightY
+  );
+  quad(
+    mouseX, mouseY,
+    mouseX + centerWeightX, mouseY,
+    width, height,
+    mouseX, mouseY + centerWeightY
+  );
+  quad(
+    mouseX, mouseY,
+    mouseX - centerWeightX, mouseY,
+    0, height,
+    mouseX, mouseY + centerWeightY
+  );
+}
