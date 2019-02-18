a373cf98613ebd91dc69c3f08a631bc2bc0d30f0
Arlo Guthrie <aguthrie@instructure.com>
2019-02-17 19:43:02 -0600

    smaller thumbs
    resolves #10

--- scripts/share.js
+++ scripts/share.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
-const path = require("path");
 const ghpages = require("gh-pages");
 const express = require("express");
 const puppeteer = require("puppeteer");
+const sharp = require("sharp");
 const imagemin = require("imagemin");
 const imageminOptipng = require("imagemin-optipng");
 const copydir = require("copy-dir");
@@ -22,22 +22,29 @@ app.use(
 );
 const http = require("http").Server(app);
 
+!fs.existsSync("./dist/thumbnails") && fs.mkdirSync("./dist/thumbnails");
+
 http.listen(port, () => {
   const sketchFiles = getSketches();
 
   (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    const toOptimize = [];
+
     await page.setViewport({
       width: 660,
-      height: 840
+      height: 840,
+      deviceScaleFactor: 2
     });
     for (let index = 0; index < sketchFiles.length; index++) {
       const sketch = sketchFiles[index];
-      const thumbnailFile = `./thumbnails/${sketch}.png`;
+      const imageFile = `./thumbnails/${sketch}.png`;
+      const thumbnailFile = `./thumbnails/${sketch}_thumb.png`;
 
-      if (!fs.existsSync(thumbnailFile)) {
-        console.log(`🖼  Generating thumbnail for ${sketch}`);
+      if (!fs.existsSync(imageFile)) {
+        console.log(`🖼  Generating thumbnails for ${sketch}...`);
+        toOptimize.push(imageFile);
 
         await page.goto(`http://localhost:8080/sketch/${sketch}`);
         try {
@@ -47,22 +54,27 @@ http.listen(port, () => {
         } catch (error) {}
         try {
           await page.screenshot({
-            path: thumbnailFile
+            path: imageFile
           });
         } catch (error) {
           throw error;
         }
-
-        await imagemin([thumbnailFile], "./thumbnails", {
-          plugins: [imageminOptipng()]
-        });
       }
+      if (!fs.existsSync(thumbnailFile)) {
+        toOptimize.push(imageFile);
+        await sharp(imageFile)
+          .resize(416, 530)
+          .toFile(thumbnailFile);
+      }
+
+      await imagemin(toOptimize, "./thumbnails", {
+        plugins: [imageminOptipng()]
+      });
     }
 
     await browser.close();
 
     await console.log("📡  Publishing to GitHub...");
-    !fs.existsSync("./dist/thumbnails") && fs.mkdirSync("./dist/thumbnails");
     copydir.sync("./thumbnails", "./dist/thumbnails");
     await ghpages.publish("./dist", error => {
       if (error) throw error;
