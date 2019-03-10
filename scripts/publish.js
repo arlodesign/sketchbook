9d9b502a79233e0bd0aa368b2450b55edf260014
Arlo Guthrie <aguthrie@instructure.com>
2019-03-10 09:19:42 -0500

    publish script

---
+++ scripts/publish.js
@@ -0,0 +1,16 @@
+const { promisify } = require("util");
+const publish = promisify(require('gh-pages').publish);
+const exec = promisify(require("child_process").exec);
+
+const DateObj = new Date();
+
+(async () => {
+  await exec('git stash --include-untracked');
+  console.log('🛠  Building website...');
+  await exec('yarn build');
+  console.log('📡  Publishing to GitHub...')
+  await publish('./public');
+  await exec('git stash pop');
+  console.log('✅  Published.')
+})();
+
