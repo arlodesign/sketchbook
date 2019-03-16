7b1479fe7159da0ec0d9211fc0846920cd4dd6ed
Arlo Guthrie <aguthrie@instructure.com>
2019-03-16 14:34:52 -0500

    2019-03-17

---
+++ src/pages/sketch/2019/03/17.js
@@ -0,0 +1,78 @@
+import React from "react";
+import SketchLayout from "~components/sketch-layout";
+import polarToCartesian from "~lib/polarToCartesian";
+
+const sketch = function(p) {
+  const GRID_SIZE = 60;
+  let hue;
+
+  let manualControl = false;
+
+  p.setup = function() {
+    p.createCanvas(660, 840);
+    p.colorMode(p.HSB, 100);
+    hue = p.random(0, 45);
+    p.fill(hue, 100, 75);
+    p.noStroke();
+  };
+
+  p.draw = function() {
+    p.background(100 - hue, 100, 50);
+
+    for (let gx = GRID_SIZE / 2; gx < p.width; gx += GRID_SIZE) {
+      for (let gy = GRID_SIZE / 2; gy < p.height; gy += GRID_SIZE) {
+        p.push();
+        p.translate(gx, gy);
+
+        let focus = manualControl
+          ? p.createVector(p.mouseX, p.mouseY)
+          : p.createVector(
+              ...polarToCartesian(
+                p.width / 2,
+                p.height / 2,
+                p.frameCount / 100,
+                p.width * p.noise(p.frameCount / 1000),
+                true
+              )
+            );
+
+        focus.sub(p.createVector(gx, gy));
+        focus.setMag(0.9 * p.min(GRID_SIZE, p.dist(0, 0, focus.x, focus.y)));
+
+        p.beginShape();
+        p.vertex(focus.x, focus.y);
+        const handles = [
+          p.createVector(focus.y, -focus.x).mult(0.333),
+          p.createVector(-focus.y, focus.x).mult(0.333),
+        ];
+        p.quadraticVertex(handles[0].x, handles[0].y, 0, 0);
+        p.quadraticVertex(handles[1].x, handles[1].y, focus.x, focus.y);
+        p.endShape(p.CLOSE);
+        p.pop();
+      }
+    }
+  };
+
+  p.mousePressed = function() {
+    manualControl = !manualControl;
+  };
+
+  p.touchMoved = function() {
+    if (
+      p.mouseX >= 0 &&
+      p.mouseY >= 0 &&
+      p.mouseX <= p.width &&
+      p.mouseY <= p.height
+    ) {
+      return false;
+    }
+  };
+};
+
+export default ({ location }) => (
+  <SketchLayout
+    sketch={sketch}
+    path={location.pathname}
+    description="Learning vector math. Click or tap on the canvas to control the focal point. Refresh page for a different color variation."
+  />
+);
