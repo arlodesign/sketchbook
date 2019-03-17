56f5c6be84ed8bb6840fd7341278e34513d20e8d
Arlo Guthrie <aguthrie@instructure.com>
2019-03-17 13:21:31 -0500

    delete .cache before publishing

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,16 +1,16 @@
 const { promisify } = require("util");
-const publish = promisify(require('gh-pages').publish);
+const publish = promisify(require("gh-pages").publish);
 const exec = promisify(require("child_process").exec);
 
 const DateObj = new Date();
 
 (async () => {
-  await exec('git stash --include-untracked');
-  console.log('🛠  Building website...');
-  await exec('yarn build');
-  console.log('📡  Publishing to GitHub...')
-  await publish('./public');
-  await exec('git stash pop');
-  console.log('✅  Published.')
+  await exec("git stash --include-untracked");
+  console.log("🛠  Building website...");
+  await exec("rm -rf .cache");
+  await exec("yarn build");
+  console.log("📡  Publishing to GitHub...");
+  await publish("./public");
+  await exec("git stash pop");
+  console.log("✅  Published.");
 })();
-
