98c81b8b0a27a90cd209fd8e510b83d3378ef756
Arlo Guthrie <aguthrie@instructure.com>
2019-03-19 15:40:12 -0500

    Updates

---
+++ component---src-pages-sketch-2018-11-24-js-13467726551d04952cdb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{149:function(t,e,n){"use strict";n.r(e);var a=n(0),r=n.n(a),o=n(251),c=function(t){t.setup=function(){t.createCanvas(660,840),t.background("white")},t.draw=function(){t.rectMode(t.CENTER),t.strokeWeight(3);for(var e=0;e<60;e++)t.translate(e,e/2),t.rotate(t.PI/e),t.rect(e+200,e+10,t.width/3,t.height/3)}};e.default=function(t){var e=t.location;return r.a.createElement(o.a,{sketch:c,path:e.pathname})}}}]);
+//# sourceMappingURL=component---src-pages-sketch-2018-11-24-js-13467726551d04952cdb.js.map
