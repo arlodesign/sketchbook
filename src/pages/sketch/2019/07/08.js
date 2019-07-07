cc5af878af54393a6ea038cd9d33a94f7e8521cd
Arlo Bryan Guthrie <mail@arlo.me>
2019-07-07 10:29:17 -0500

    2019-07-08

---
+++ src/pages/sketch/2019/07/08.js
@@ -0,0 +1,54 @@
+import React from "react";
+import SketchLayout from "~components/sketch-layout";
+
+const RENDER = false;
+
+const sketch = function(p) {
+  const GRID = 30;
+  const FRAMES = 600;
+  const QTY = 3;
+  const HUE = p.random(1 / QTY);
+  const COLORS = Array.from({ length: QTY }, (_, i) => HUE + (1 / QTY) * i);
+
+  p.setup = function() {
+    if (RENDER) {
+      p.pixelDensity(1);
+      p.frameRate(30);
+    }
+    p.createCanvas(660, 840);
+    p.createLoop(10, {
+      gif: RENDER ? { render: false, open: true } : false,
+      noiseRadius: 1,
+    });
+    p.colorMode(p.HSB, 1);
+  };
+
+  p.draw = function() {
+    p.blendMode(p.BLEND);
+    p.background(0);
+    p.blendMode(p.HARD_LIGHT);
+
+    for (let i = 0; i <= p.width; i += GRID) {
+      COLORS.forEach(c => {
+        p.push();
+        p.translate(i, 0);
+        p.stroke(c, 0.5, 1);
+        p.strokeWeight(p.map(p.cos(p.animLoop.theta), -1, 1, 1, GRID / 2));
+
+        const x = p.map(
+          p.cos(p.animLoop.theta),
+          -1,
+          1,
+          0,
+          p.animLoop.noise2D(i, c) * GRID * 3
+        );
+        p.line(x, 0, x, p.height);
+        p.pop();
+      });
+    }
+  };
+};
+
+export default ({ location }) => (
+  <SketchLayout sketch={sketch} path={location.pathname} description="" />
+);
