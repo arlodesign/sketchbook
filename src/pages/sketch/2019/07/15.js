a705f7cd9a6a8ce4f7c321c5d008b51ea5cd4adf
Arlo Bryan Guthrie <mail@arlo.me>
2019-07-16 09:55:17 -0500

    2019-07-15

---
+++ src/pages/sketch/2019/07/15.js
@@ -0,0 +1,46 @@
+import React from "react";
+import SketchLayout from "~components/sketch-layout";
+import { spherical } from "coordinate-systems";
+
+import "p5.createloop";
+
+const RENDER = process.env.DEV && false;
+
+const sketch = function(p) {
+  let size;
+  let count;
+
+  p.setup = function() {
+    if (RENDER) {
+      p.pixelDensity(1);
+      p.frameRate(30);
+    }
+
+    p.createCanvas(660, 840, p.WEBGL);
+    p.noFill();
+
+    size = p.width / 2;
+
+    p.createLoop(60, {
+      gif: RENDER ? { render: false, open: true } : false,
+    });
+  };
+
+  p.draw = function() {
+    p.background(128);
+
+    for (let i = 0; i < size; i++) {
+      p.push();
+      p.stroke(p.map(i, 0, size, 255, 0));
+      p.rotateX(p.cos(p.animLoop.theta) * (p.TWO_PI * (i / size)));
+      p.rotateY(p.sin(p.animLoop.theta) * (p.TWO_PI * (i / size)));
+      p.rotateZ(p.sin(p.animLoop.theta) * (p.TWO_PI * (i / size)));
+      p.box(i, i, i);
+      p.pop();
+    }
+  };
+};
+
+export default ({ location }) => (
+  <SketchLayout sketch={sketch} path={location.pathname} description="" />
+);
