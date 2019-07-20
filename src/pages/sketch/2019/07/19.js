efa84883a61af66235cf7e99e64d7dee2f634583
Arlo Bryan Guthrie <mail@arlo.me>
2019-07-19 19:33:01 -0500

    2019-07-19

---
+++ src/pages/sketch/2019/07/19.js
@@ -0,0 +1,59 @@
+import React from "react";
+import SketchLayout from "~components/sketch-layout";
+
+const sketch = function(p) {
+  const HUE = p.random(0.5, 1);
+  const ITERATIONS = 30;
+
+  let a = 0;
+  let i = 1;
+  let x = 0;
+
+  p.setup = function() {
+    p.createCanvas(660, 840);
+    p.colorMode(p.HSB, 1);
+    p.background(HUE, 0.8, 0.1);
+    p.noFill();
+    p.blendMode(p.OVERLAY);
+    p.ellipseMode(p.CENTER);
+  };
+
+  p.draw = function() {
+    const NOISE = p.noise(p.frameCount / (ITERATIONS * 100), x);
+
+    p.push();
+    p.stroke(
+      p.map(p.cos(p.frameCount / (ITERATIONS * 10)), -1, 1, HUE, HUE - 0.5),
+      0.5,
+      0.8,
+      NOISE
+    );
+    p.translate(x, p.height / 2);
+    p.arc(
+      0,
+      0,
+      50,
+      p.height * NOISE,
+      p.frameCount / ITERATIONS,
+      p.frameCount / ITERATIONS + p.QUARTER_PI
+    );
+    p.pop();
+    x++;
+
+    console.log(i);
+
+    a -= p.TWO_PI / (p.height * ITERATIONS);
+    if (x > p.width) {
+      x = 0;
+      i++;
+      if (i > ITERATIONS) {
+        p.noLoop();
+        console.log("done");
+      }
+    }
+  };
+};
+
+export default ({ location }) => (
+  <SketchLayout sketch={sketch} path={location.pathname} description="" />
+);
