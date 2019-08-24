e1eddeaa8b56080bb8496e9d2cf378303acdc866
Arlo Bryan Guthrie <mail@arlo.me>
2019-08-24 18:16:10 -0500

    2019-08-23 and 24

---
+++ src/pages/sketch/2019/08/23.js
@@ -0,0 +1,79 @@
+import React from "react";
+import SketchLayout from "~components/sketch-layout";
+import "p5.createloop";
+import polarToCartesian from "~lib/polarToCartesian";
+
+const RENDER = process.env.DEV && false;
+const DURATION = 120;
+
+const sketch = function(p) {
+  p.setup = function() {
+    p.frameRate(30);
+
+    if (RENDER) {
+      p.pixelDensity(1);
+    }
+
+    p.createCanvas(660, 840);
+    p.background(255);
+    p.noFill();
+    p.blendMode(p.MULTIPLY);
+
+    p.createLoop(DURATION, {
+      gif: RENDER ? { render: false, open: true } : false,
+    });
+  };
+
+  p.draw = function() {
+    const { noise1D, noise2D, theta } = p.animLoop;
+
+    ["cyan", "magenta", "yellow"].forEach((c, i) => {
+      const clr = p.color(c);
+      clr.setAlpha(16);
+      p.stroke(clr);
+
+      p.push();
+      p.translate(
+        ...polarToCartesian(
+          p.width / 3,
+          p.height / 2,
+          theta + i * (p.TWO_PI / 3),
+          p.map(
+            p.cos(theta + i * (p.TWO_PI / 3)),
+            -1,
+            1,
+            p.height / 2,
+            0,
+            true
+          ),
+          true
+        )
+      );
+      p.rotate(
+        p.map(noise1D((i + 1) / 100, { radius: 0.1 }), -1, 1, p.TWO_PI, 0)
+      );
+      const ellipseWidth = p.map(
+        noise2D(c.length, i + 1, { radius: 2 }),
+        -1,
+        1,
+        p.width / 2,
+        p.width
+      );
+      const ellipseHeight = p.map(
+        noise2D(i + 1, c.length, { radius: 2 }),
+        -1,
+        1,
+        p.height / 4,
+        p.height / 8
+      );
+      p.ellipse(0, 0, ellipseWidth, ellipseHeight);
+      p.pop();
+    });
+
+    !RENDER && p.frameCount === DURATION * 30 && p.noLoop();
+  };
+};
+
+export default ({ location }) => (
+  <SketchLayout sketch={sketch} path={location.pathname} description="" />
+);
