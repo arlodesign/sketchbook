c0465fc0aa7a3cfa90c8df794129ab52e0be262d
Arlo Bryan Guthrie <mail@arlo.me>
2019-08-30 20:26:54 -0500

    2019-08-30

---
+++ src/pages/sketch/2019/08/30.js
@@ -0,0 +1,80 @@
+import React from "react";
+import SketchLayout from "~components/sketch-layout";
+import "p5.createloop";
+import * as ease from "~lib/easing";
+import polarToCartesian from "~lib/polarToCartesian";
+
+const easings = [
+  ease.easeInQuad,
+  ease.easeOutQuad,
+  ease.easeInOutQuad,
+  ease.easeInCubic,
+  ease.easeOutCubic,
+  ease.easeInOutCubic,
+  ease.easeInQuart,
+  ease.easeOutQuart,
+  ease.easeInOutQuart,
+  ease.easeInQuint,
+  ease.easeOutQuint,
+  ease.easeInOutQuint,
+];
+
+const RENDER = process.env.DEV && false;
+const DURATION = 30;
+
+const sketch = function(p) {
+  p.frameRate(60);
+
+  p.setup = function() {
+    if (RENDER) {
+      p.pixelDensity(1);
+    }
+
+    p.createCanvas(660, 840);
+    p.background(255);
+    p.noFill();
+    p.strokeWeight(10);
+    p.strokeJoin(p.ROUND);
+    p.blendMode(p.MULTIPLY);
+
+    p.createLoop(DURATION, {
+      gif: RENDER ? { render: false, open: true } : false,
+    });
+  };
+
+  p.draw = function() {
+    const { noise1D, theta } = p.animLoop;
+
+    let c;
+    switch (p.frameCount % 3) {
+      case 0:
+        c = p.color("cyan");
+        break;
+      case 1:
+        c = p.color("magenta");
+        break;
+      case 2:
+        c = p.color("yellow");
+        break;
+    }
+    p.stroke(c);
+
+    easings.forEach((e, i) => {
+      const d = p.map(e(p.abs(p.cos(theta))), 0, 1, 0, p.width);
+      p.point(
+        ...polarToCartesian(
+          p.width / 2,
+          p.height / 2,
+          theta + (p.TWO_PI / easings.length) * i,
+          d * noise1D(i + 1),
+          true
+        )
+      );
+    });
+    p.frameCount === DURATION * 60 && !RENDER && p.noLoop();
+  };
+};
+
+export default ({ location }) => (
+  <SketchLayout sketch={sketch} path={location.pathname} description="" />
+);
