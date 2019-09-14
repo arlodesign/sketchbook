d3d0da7f1ce3ce9f59ce9ae4d75804bb8d405312
Arlo Bryan Guthrie <mail@arlo.me>
2019-09-13 20:35:19 -0500

    2019-09-12

---
+++ src/pages/sketch/2019/09/12.js
@@ -0,0 +1,148 @@
+import React from "react";
+import SketchLayout from "~components/sketch-layout";
+import { easeInOutQuad as ease } from "~lib/easing";
+import "p5.createloop";
+
+const RENDER = process.env.DEV && false;
+const GRID = 30;
+
+const sketch = function(p) {
+  p.setup = function() {
+    p.createCanvas(660, 840);
+    p.noStroke();
+    p.colorMode(p.HSL, 1);
+    p.rectMode(p.CENTER);
+
+    p.createLoop(3, {
+      gif: RENDER ? { render: false, open: true } : false,
+      noiseRadius: 0.5,
+    });
+  };
+
+  p.draw = function() {
+    const { theta } = p.animLoop;
+
+    p.blendMode(p.BLEND);
+    p.background(255);
+    p.blendMode(p.MULTIPLY);
+
+    for (let x = 0; x < p.width / GRID; x++) {
+      for (let y = 0; y < p.height / GRID; y++) {
+        p.push();
+        p.fill("cyan");
+        p.translate(x * GRID + GRID / 2, y * GRID + GRID / 2);
+        p.square(
+          0,
+          0,
+          ease(
+            p.map(
+              p.cos(
+                p.dist(
+                  x * GRID + GRID / 2,
+                  y * GRID + GRID / 2,
+                  p.width / 2,
+                  p.height / 2
+                ) + theta
+              ),
+              -1,
+              1,
+              0,
+              1
+            )
+          ) * GRID,
+          ease(
+            p.map(
+              p.sin(
+                p.dist(
+                  x * GRID + GRID / 2,
+                  y * GRID + GRID / 2,
+                  p.width / 2,
+                  p.height / 2
+                ) + theta
+              ),
+              -1,
+              1,
+              0,
+              1
+            )
+          ) * GRID
+        );
+        p.pop();
+
+        p.push();
+        p.fill("yellow");
+        p.translate(p.width - (x * GRID + GRID / 2), y * GRID + GRID / 2);
+        p.square(
+          0,
+          0,
+          ease(
+            p.map(
+              p.sin(
+                p.dist(
+                  x * GRID + GRID / 2,
+                  y * GRID + GRID / 2,
+                  p.width / 2,
+                  p.height / 2
+                ) + theta
+              ),
+              -1,
+              1,
+              0,
+              1
+            )
+          ) * GRID,
+          ease(
+            p.map(
+              p.cos(
+                p.dist(
+                  x * GRID + GRID / 2,
+                  y * GRID + GRID / 2,
+                  p.width / 2,
+                  p.height / 2
+                ) + theta
+              ),
+              -1,
+              1,
+              0,
+              1
+            )
+          ) * GRID
+        );
+        p.pop();
+
+        p.push();
+        p.fill("magenta");
+        p.translate(x * GRID - GRID / 2, y * GRID + GRID / 2);
+        p.square(
+          0,
+          0,
+          ease(
+            p.map(
+              p.sin(p.dist(x, y, -p.width / 2, p.height / 2) + theta),
+              -1,
+              1,
+              1,
+              0
+            )
+          ) *
+            (GRID / 2),
+          ease(
+            p.map(
+              p.cos(p.dist(x, y, -p.width / 2, p.height / 2) + theta),
+              -1,
+              1,
+              1,
+              0
+            )
+          ) *
+            (GRID / 2)
+        );
+        p.pop();
+      }
+    }
+  };
+};
+
+export default ({ location }) => (
+  <SketchLayout sketch={sketch} path={location.pathname} description="" />
+);
