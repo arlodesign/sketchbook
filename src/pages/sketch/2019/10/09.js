dacfbe9f33f79e0b7c602f6158b3d878132a3d27
Arlo Bryan Guthrie <mail@arlo.me>
2019-10-09 20:56:20 -0500

    2019-10-08 to 09

---
+++ src/pages/sketch/2019/10/09.js
@@ -0,0 +1,69 @@
+import React from "react";
+import SketchLayout from "~components/sketch-layout";
+import { easeInOutQuint as ease } from "~lib/easing";
+import "p5.createloop";
+import polarToCartesian from "~lib/polarToCartesian";
+
+const RENDER = process.env.DEV && false;
+const RINGS = 10;
+
+const sketch = function(p) {
+  const HUE = p.random(50);
+  let ringSize;
+
+  p.setup = function() {
+    if (RENDER) {
+      p.pixelDensity(1);
+      p.frameRate(30);
+    }
+
+    p.createCanvas(660, 840);
+    p.background(255);
+    p.colorMode(p.HSB, 100);
+    p.strokeCap(p.SQUARE);
+    p.createLoop(60, {
+      gif: RENDER ? { render: false, open: true } : false,
+    });
+
+    ringSize = p.dist(0, 0, p.width / 2, p.height / 2) / RINGS;
+  };
+
+  p.draw = function() {
+    const { theta, noise1D } = p.animLoop;
+    p.translate(p.width / 2, p.height / 2);
+    p.rotate(p.QUARTER_PI);
+    for (let r = 0; r < RINGS; r++) {
+      const element = RINGS;
+      const lerpVal = p.map(p.sin(theta * (r + 1)), -1, 1, 1, 0);
+      const start = p.TWO_PI * (r / RINGS);
+      const pointA = polarToCartesian(
+        0,
+        0,
+        theta * (r % 2 ? 1 : -1),
+        ringSize * r,
+        true
+      );
+      const pointB = polarToCartesian(
+        0,
+        0,
+        theta * (r % 2 ? 1 : -1),
+        ringSize * (r + 1),
+        true
+      );
+
+      p.stroke(
+        p.lerpColor(
+          p.color(HUE, 100, 25),
+          p.color(HUE + 50, 25, 100),
+          ease(lerpVal)
+        )
+      );
+      p.strokeWeight(p.map(r, 0, RINGS, 1, 7));
+      p.line(...pointA, ...pointB);
+    }
+  };
+};
+
+export default ({ location }) => (
+  <SketchLayout sketch={sketch} path={location.pathname} description="" />
+);
