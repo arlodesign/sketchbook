e19ee58bdbd00058a32f47f08b053963ac7d2bce
Arlo Bryan Guthrie <mail@arlo.me>
2020-01-13 18:58:20 -0600

    2020-01-13

---
+++ src/pages/sketch/2020/01/13.js
@@ -0,0 +1,70 @@
+import React from "react";
+import SketchLayout from "~components/sketch-layout";
+import "p5.createloop";
+
+const sketch = function(p) {
+  const RENDER = p.getURLParams().render;
+  const DURATION = 49;
+  const RATE = 30;
+  const MULTIPLIERS = p.shuffle([1, 2, 3, 5, 7, 11]);
+  const WEIGHT = 2;
+
+  p.setup = function() {
+    p.pixelDensity(1);
+    p.frameRate(RATE);
+    p.createCanvas(660, 840);
+    p.createLoop(DURATION, {
+      gif: RENDER ? { render: false, open: true } : false,
+    });
+    p.strokeWeight(WEIGHT);
+    p.noSmooth();
+    p.strokeCap(p.SQUARE);
+  };
+
+  p.draw = function() {
+    p.blendMode(p.BLEND);
+    p.background(255);
+    p.blendMode(p.MULTIPLY);
+
+    for (let y = 0; y < p.height; y += WEIGHT * 2) {
+      const a = p.map(
+        y,
+        0,
+        p.height,
+        p.animLoop.theta,
+        p.animLoop.theta + p.TWO_PI
+      );
+
+      p.stroke("cyan");
+      p.line(
+        p.map(p.sin(a * MULTIPLIERS[0]), -1, 1, 0, p.width),
+        y,
+        p.map(p.cos(a * MULTIPLIERS[1]), -1, 1, 0, p.width),
+        y
+      );
+
+      p.stroke("magenta");
+      p.line(
+        p.map(p.sin(a * MULTIPLIERS[2]), -1, 1, 0, p.width),
+        y + WEIGHT / 2,
+        p.map(p.cos(a * MULTIPLIERS[3]), -1, 1, 0, p.width),
+        y + WEIGHT / 2
+      );
+      p.stroke("yellow");
+      p.line(
+        p.map(p.sin(a * MULTIPLIERS[4]), -1, 1, 0, p.width),
+        y + WEIGHT,
+        p.map(p.cos(a * MULTIPLIERS[5]), -1, 1, 0, p.width),
+        y + WEIGHT
+      );
+    }
+
+    // p.frameCount % RATE === 0 &&
+    //   console.log(`${p.frameCount} / ${DURATION * RATE}`);
+    // !RENDER && p.frameCount === DURATION * RATE && p.noLoop();
+  };
+};
+
+export default ({ location }) => (
+  <SketchLayout sketch={sketch} path={location.pathname} description="" />
+);
