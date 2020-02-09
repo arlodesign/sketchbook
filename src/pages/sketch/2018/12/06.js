cda7889975f4d7a65c7d6d92ee86b64cfc4cc16c
Arlo Bryan Guthrie <mail@arlo.me>
2020-02-09 10:04:36 -0600

    2018-12-06 v2

--- src/pages/sketch/2018/12/06.js
+++ src/pages/sketch/2018/12/06.js
@@ -1,19 +1,19 @@
 import React from "react";
+import styled from "@emotion/styled";
 import SketchLayout from "~components/sketch-layout";
 
 const sketch = function(p) {
   const speed = 6;
   let scalar;
-  let jump = 0;
   let angle = 0;
   let size;
-  let letsDraw = false;
+  let clr = "pink";
 
   p.setup = function() {
     p.createCanvas(660, 840);
     p.background("white");
+    p.colorMode(p.HSB, 1000);
     p.noStroke();
-    p.fill("rgba(0, 0, 0, .9)");
     scalar = p.random(10, 20);
     size = p.random(10, 20);
   };
@@ -22,7 +22,9 @@ const sketch = function(p) {
     const x = p.mouseX + p.cos(angle) * scalar;
     const y = p.mouseY + p.sin(angle) * scalar;
     const noiseVal = p.noise(x, y);
-    if (x > 0 && y > 0 && x < p.width && y < p.height && p.mouseIsPressed) {
+    clr === "rainbow" ? p.fill(p.frameCount % 1000, 1000, 1000) : p.fill(clr);
+
+    if (x > 0 && y > 0 && x < p.width && y < p.height) {
       p.ellipse(x, y, size * noiseVal);
     }
     angle += speed;
@@ -38,8 +40,93 @@ const sketch = function(p) {
       return false;
     }
   };
+
+  p.keyReleased = function() {
+    switch (p.key) {
+      case "p":
+        clr = "pink";
+        break;
+
+      case "r":
+        clr = "red";
+        break;
+
+      case "u":
+        clr = "purple";
+        break;
+
+      case "b":
+        clr = "blue";
+        break;
+
+      case "g":
+        clr = "green";
+        break;
+
+      case "o":
+        clr = "orange";
+        break;
+
+      default:
+        clr = "rainbow";
+        break;
+    }
+  };
 };
 
+const DL = styled.dl`
+  display: grid;
+  grid-template: auto / 6ch 1fr;
+`;
+const DT = styled.dt`
+  grid-column: 1;
+`;
+const DD = styled.dd`
+  grid-column: 2;
+`;
+
 export default ({ location }) => (
-  <SketchLayout sketch={sketch} path={location.pathname} description="Click to draw." />
+  <SketchLayout
+    sketch={sketch}
+    path={location.pathname}
+    description={
+      <>
+        Updated 2020-02-09 to support color. Move the pointer our touch to draw.
+        Press keys on your keyboard to change the color.
+        <DL>
+          <DT>
+            <kbd>P</kbd>
+          </DT>
+          <DD>pink</DD>
+
+          <DT>
+            <kbd>R</kbd>
+          </DT>
+          <DD>red</DD>
+
+          <DT>
+            <kbd>U</kbd>
+          </DT>
+          <DD>purple</DD>
+
+          <DT>
+            <kbd>B</kbd>
+          </DT>
+          <DD>blue</DD>
+
+          <DT>
+            <kbd>G</kbd>
+          </DT>
+          <DD>green</DD>
+
+          <DT>
+            <kbd>O</kbd>
+          </DT>
+          <DD>orange</DD>
+          <DT>Any other key</DT>
+          <DD>rainbow</DD>
+        </DL>
+      </>
+    }
+  />
 );
