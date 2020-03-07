e5218180e19fc231d9144e89dc365b27820a28c0
Arlo Bryan Guthrie <mail@arlo.me>
2020-03-07 11:09:16 -0600

    Responsive canvas size determined from sketch

--- src/components/sketch.js
+++ src/components/sketch.js
@@ -1,23 +1,30 @@
 import React from "react";
-import styled from "@emotion/styled";
+import { css } from "@emotion/core";
 
-const SketchWrapper = styled.div`
-  width: 100%;
-  overflow: hidden;
-  position: relative;
-  height: 0;
-  padding: 0 0 127.27272727%;
+const SketchWrapper = React.forwardRef(({ width, height }, ref) => {
+  return (
+    <div
+      ref={ref}
+      css={css`
+        width: 100%;
+        overflow: hidden;
+        position: relative;
+        height: 0;
+        padding: 0 0 ${(height / width) * 100}%;
 
-  & > canvas {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    width: 100% !important;
-    height: 100% !important;
-  }
-`;
+        & > canvas {
+          position: absolute;
+          top: 0;
+          right: 0;
+          bottom: 0;
+          left: 0;
+          width: 100% !important;
+          height: 100% !important;
+        }
+      `}
+    />
+  );
+});
 
 class Sketch extends React.Component {
   constructor(props) {
@@ -25,8 +32,16 @@ class Sketch extends React.Component {
     this.sketchRef = React.createRef();
     this.canvas = null;
   }
+  state = {
+    width: 1,
+    height: 1,
+  };
   componentDidMount() {
     this.canvas = new p5(this.props.sketch, this.sketchRef.current);
+    this.setState({
+      width: this.canvas.width,
+      height: this.canvas.height,
+    });
   }
   componentDidUpdate() {
     this.canvas.remove();
@@ -36,7 +51,7 @@ class Sketch extends React.Component {
     this.canvas.remove();
   }
   render() {
-    return <SketchWrapper ref={this.sketchRef} />;
+    return <SketchWrapper ref={this.sketchRef} {...this.state} />;
   }
 }
 
