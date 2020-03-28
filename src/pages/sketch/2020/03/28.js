660f0d84b568117fe1b368eb7f628abf451ad1d8
Arlo Bryan Guthrie <mail@arlo.me>
2020-03-28 14:20:18 -0500

    2020-03-24 & the real 28

--- src/pages/sketch/2020/03/28.js
+++ src/pages/sketch/2020/03/28.js
@@ -1,140 +1,112 @@
 import React from "react";
 import SketchLayout from "~components/sketch-layout";
 import "p5.createloop";
-import { easeOutQuint as ease } from "~lib/easing";
-import lerpLine from "~lib/lerpLine";
+import { easeOutQuad as ease } from "~lib/easing";
 
 const sketch = function(p) {
-  const RENDER = p.getURLParams().render;
-  const DURATION = 60;
-  const RATE = 60;
-  const G = 100;
-  const HUE = p.random(0.2, 0.8);
-
-  let attractors = [];
+  let size = 0;
+  let socialDistance = 0;
   let particles = [];
-  let alpha = 0;
-
-  class Attractor {
-    constructor(dir) {
-      this.x = p.randomGaussian(p.width / 2, p.width / 10);
-      this.y = p.constrain(
-        p.random(p.height / 2, p.height / 3),
-        p.height * 0.333,
-        p.height * 0.666
-      );
-      this.dir = dir;
-      this.start = p.random(p.TWO_PI);
-    }
-    get pos() {
-      return p.createVector(this.x, this.y);
-    }
-  }
+  let id = 0;
+  let moved = 0;
 
   class Particle {
-    constructor(sat) {
-      this.pos = p.createVector(
-        p.randomGaussian(p.width / 2, p.width / 3),
-        p.randomGaussian(p.height / 2, p.width / 3)
-      );
-      this.vel = p5.Vector.random2D();
+    constructor(x, y) {
+      this.id = ++id;
+      this.pos = p.createVector(x, y);
+      this.dir = p.createVector(0, 0);
       this.acc = p.createVector();
-      this.prev = null;
-      this.sat = sat;
+      this.trails = [];
+    }
+    draw() {
+      p.noStroke();
+      p.fill(255, 0, 255, 32);
+      this.trails.forEach(pos => p.ellipse(...pos, size));
+
+      p.fill(this.id % 2 ? "pink" : "orange");
+      p.ellipse(this.pos.x, this.pos.y, size);
+    }
+    closestParticles() {
+      return particles
+        .filter(({ id }) => id !== this.id)
+        .filter(
+          ({ pos }) =>
+            p.dist(pos.x, pos.y, this.pos.x, this.pos.y) < socialDistance
+        )
+        .sort(
+          ({ pos: aPos }, { pos: bPos }) =>
+            p.dist(aPos.x, aPos.y, this.pos.x, this.pos.y) -
+            p.dist(bPos.x, bPos.y, this.pos.x, this.pos.y)
+        );
     }
     update() {
-      this.pos.add(this.vel);
-      this.vel.add(this.acc);
-    }
-    show() {
-      if (this.prev) {
-        let closestAttractorDist = 1000000;
-        let closestAttractor = null;
-        attractors.forEach(attractor => {
-          const thisDist = p.dist(
-            attractor.pos.x,
-            attractor.pos.y,
-            this.pos.x,
-            this.pos.y
-          );
-          if (thisDist < closestAttractorDist) {
-            closestAttractorDist = thisDist;
-            closestAttractor = attractor;
-          }
-        });
-        p.stroke(
-          HUE + p.animLoop.noise1D(this.sat) * 0.2,
-          this.sat,
-          p.map(closestAttractorDist, 0, p.width / 2, 1, 0, true),
-          alpha
-        );
-        lerpLine(
-          p,
-          this.pos.x,
-          this.pos.y,
-          closestAttractor.pos.x,
-          closestAttractor.pos.y,
-          (p.animLoop.noise() + 1) / 4,
-          false,
-          false
-        );
-        p.line(this.pos.x, this.pos.y, this.prev.x, this.prev.y);
-      }
-      this.prev = { x: this.pos.x, y: this.pos.y };
-    }
-    attracted(target) {
-      let force = p5.Vector.sub(target.pos, this.pos);
-      force.mult(target.dir);
-      let dsquared = force.magSq();
-      dsquared = p.min(dsquared, G * 20);
-      const strength = G / dsquared;
-      force.setMag(strength);
-      this.acc = force;
+      const cps = this.closestParticles();
+      this.trails.unshift([this.pos.x, this.pos.y]);
+      this.trails = this.trails.slice(0, 5);
+
+      cps.forEach(cp => {
+        moved++;
+
+        const direction = p5.Vector.sub(cp.pos, this.pos);
+        direction.rotate(p.PI);
+        direction.normalize();
+        this.dir.add(direction);
+        this.dir.normalize();
+        this.pos.add(this.dir);
+
+        const force = p5.Vector.mult(this.dir, this.pos);
+        const dist = p.dist(cp.pos.x, cp.pos.y, this.pos.x, this.pos.y);
+        const mag = ease((socialDistance - dist) / socialDistance);
+        force.setMag(mag);
+        this.acc.add(force);
+
+        this.pos.add(this.acc);
+      });
     }
   }
 
-  p.setup = function() {
-    p.pixelDensity(RENDER ? 1 : 2);
-    p.frameRate(RATE);
-    p.createCanvas(660, 840);
-    p.colorMode(p.HSB, 1);
-    p.background(1 - HUE, 0.2, 0.8);
-    p.blendMode(p.OVERLAY);
-    p.createLoop(DURATION, {
-      gif: RENDER ? { render: false, open: true } : false,
-      noiseRadius: 0.1,
-    });
-    p.strokeWeight(2);
+  function pushParticle() {
+    particles.push(new Particle(p.mouseX, p.mouseY));
+  }
 
-    attractors = Array(10)
-      .fill()
-      .map((_, i) => new Attractor(i % 4 ? 1 : -1));
-    particles = Array(3)
-      .fill()
-      .map((_, i) => new Particle((i + 1) / 3));
+  p.mousePressed = pushParticle;
+  p.mouseDragged = pushParticle;
+
+  p.setup = function() {
+    p.pixelDensity(1);
+    p.createCanvas(660, 840);
+    p.noSmooth();
+    particles.push(new Particle(p.width / 2, p.height / 2));
+    size = p.width / 50;
+    socialDistance += p.width / 10;
   };
 
   p.draw = function() {
-    const { progress } = p.animLoop;
-    const easedProgress = ease(progress);
-    alpha = -4 * easedProgress * easedProgress + 4 * easedProgress;
+    p.background(0);
+
+    moved = 0;
 
     particles.forEach(particle => {
-      attractors.forEach(attractor => {
-        particle.attracted(attractor);
-      });
       particle.update();
-      particle.show();
+      particle.draw();
     });
-
-    p.frameCount % 100 === 0 && console.info(`${p.floor(progress * 100)}%`);
-    if (!RENDER && p.frameCount === DURATION * RATE) {
-      console.info("100%");
-      p.noLoop();
+    if (moved === 0) {
+      // remove off-canvas particles for performance
+      particles = particles.filter(
+        ({ pos }) =>
+          pos.x > -size &&
+          pos.x < p.width + size &&
+          pos.y > -size &&
+          pos.y < p.height + size
+      );
     }
   };
 };
 
 export default ({ location }) => (
-  <SketchLayout sketch={sketch} path={location.pathname} description="" />
+  <SketchLayout
+    sketch={sketch}
+    path={location.pathname}
+    description="Click and drag to maintain social distancing."
+  />
 );
