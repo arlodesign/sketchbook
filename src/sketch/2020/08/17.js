561e56bc5860fd95a8928d90873f1fe4327af93e
Arlo Bryan Guthrie <mail@arlo.me>
2020-08-16 10:35:27 -0500

    2020-08-17

--- src/sketch/2020/08/17.js
+++ src/sketch/2020/08/17.js
@@ -1,11 +1,14 @@
 import p5 from "p5";
 import "p5.createloop";
+import { easeInOutQuad, easeInOutCubic } from "util/easing";
+import polarToCartesian from "util/polarToCartesian";
 
 const sketch = function (p) {
   const RENDER = p.getURLParams().render;
-  const RENDER_SIZE = 1080;
-  const DURATION = 20;
+  const RENDER_SIZE = 540;
+  const DURATION = 55;
   const RATE = 30;
+  const HUE = p.random();
 
   p.setup = function () {
     p.pixelDensity(1);
@@ -17,20 +20,44 @@ const sketch = function (p) {
     p.background(255);
     p.noSmooth();
     p.createLoop(DURATION, {
-      noiseRadius: 0.1,
+      noiseRadius: 0.4,
     });
+    p.colorMode(p.HSL, 1);
   };
 
   p.draw = function () {
     const { progress, theta, noise1D, noise2D } = p.animLoop;
-    const loopedProgress = -4 * progress * progress + 4 * progress;
-
     p.background(255);
     p.loadPixels();
+    const loopedProgress = -2 * progress * progress + 2 * progress;
+    const focusPoint = polarToCartesian(
+      p.map(noise1D(0.5), -1, 1, 0, p.width),
+      p.map(noise1D(0.9), -1, 1, 0, p.height),
+      theta,
+      p.height / 2,
+      true
+    );
+    const maxDist = p.dist(...focusPoint, 0, 0);
 
     for (let y = 0; y < p.height; y++) {
       for (let x = 0; x < p.width; x++) {
-        let c = p.color(0);
+        const index = (x + y * p.width) * 4;
+        const xN = p.floor((x + p.width) / (p.width / 9));
+        const yN = (y + p.height) / 400;
+        const n =
+          Math.abs(noise2D(xN, yN)) * (p.dist(...focusPoint, x, y) / maxDist);
+
+        const h = p.constrain(
+          (Math.floor(n / 0.167) * 0.169 +
+            -0.344 +
+            Math.sin(n / 0.09) * 0.34 +
+            0.336) *
+            0.971 +
+            loopedProgress,
+          0,
+          1
+        );
+        const c = p.color(HUE, easeInOutCubic(h), easeInOutQuad(h));
 
         p.pixels[index] = p.red(c);
         p.pixels[index + 1] = p.green(c);
