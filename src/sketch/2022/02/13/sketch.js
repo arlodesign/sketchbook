e33d38c6aaecd44f67e66d6a4189b884d5961f23
Arlo Bryan Guthrie <mail@arlo.me>
2022-02-01 17:25:10 -0600

    2022-02-13

---
+++ src/sketch/2022/02/13/sketch.js
@@ -0,0 +1,152 @@
+import p5 from "p5";
+import attachCreateLoop from "p5.createloop/src/p5.createLoop";
+import { getLineBy2Points } from "get-parabola";
+import { easeOutQuint } from "util/easing";
+import waveLoop from "util/waveLoop";
+import polarToCartesian from "util/polarToCartesian";
+
+window.p5 = p5;
+attachCreateLoop();
+
+const slope = (c) =>
+  easeOutQuint(
+    Math.min(
+      Math.max(
+        getLineBy2Points([
+          { x: 0.017, y: 0 },
+          { x: 0.018, y: 1 },
+        ])(c),
+        0
+      ),
+      1
+    )
+  );
+
+const sketch = function (p) {
+  const RENDER = p.getURLParams().render === "true";
+  const RENDER_SIZE = 1080;
+  const TIME = 20;
+  const FRAME_RATE = 60;
+
+  // Lower if frames skip
+  const RENDER_SPEED = 5;
+  const DURATION = TIME * (RENDER ? ~~(FRAME_RATE / RENDER_SPEED) : 1);
+  const RATE = ~~(RENDER ? RENDER_SPEED : FRAME_RATE);
+  const FRAMES = DURATION * RATE;
+
+  let urlParams;
+  let hue;
+  let link;
+
+  function changeURL() {
+    const searchParams = {
+      hue: hue.value(),
+    };
+
+    link.attribute(
+      "href",
+      "?" +
+        new URLSearchParams({
+          ...searchParams,
+          render: !RENDER,
+        }).toString()
+    );
+    window.history.replaceState(
+      null,
+      document.title,
+      "?" +
+        new URLSearchParams({
+          ...searchParams,
+          render: RENDER,
+        }).toString()
+    );
+  }
+
+  p.setup = function () {
+    p.pixelDensity(1);
+    p.frameRate(RATE);
+    p.createCanvas(
+      RENDER ? RENDER_SIZE : ~~(RENDER_SIZE / 10),
+      RENDER ? RENDER_SIZE : ~~(RENDER_SIZE / 10)
+    );
+    p.background(255);
+    p.createLoop(DURATION, {
+      noiseRadius: 0.5,
+    });
+    p.noSmooth();
+
+    p.colorMode(p.HSB, 1);
+
+    urlParams = Object.assign(
+      {
+        hue: p.random(),
+      },
+      p.getURLParams()
+    );
+
+    hue = p.createSlider(0, 1, parseFloat(urlParams.hue, 10), 0.01);
+    hue.changed(changeURL);
+    link = p.createA("?", RENDER ? "Draft" : "Render");
+
+    changeURL();
+  };
+
+  p.draw = function () {
+    const { progress, theta, noise, noise1D, noise2D } = p.animLoop;
+
+    const maxDist = p.dist(0, 0, p.width, p.height);
+    const positions = [];
+
+    positions.push([0, 0]);
+    positions.push([p.width, 0]);
+    positions.push([p.width, p.height]);
+    positions.push([0, p.height]);
+    positions.push(
+      polarToCartesian(
+        p.width / 2,
+        p.height / 2,
+        p.TWO_PI * noise(),
+        p.height / 4 + (p.height / 4) * waveLoop(5),
+        true
+      )
+    );
+
+    p.background(hue.value());
+
+    p.loadPixels();
+    for (let x = 0; x < p.width; x++) {
+      for (let y = 0; y < p.height; y++) {
+        const PIX = (x + y * p.width) * 4;
+        const d = positions.reduce(
+          (prev, curr) => prev * (p.dist(x, y, ...curr) / maxDist),
+          1
+        );
+        const s = Math.abs(
+          noise2D(x / (RENDER ? 1000 : 100), y / (RENDER ? 1000 : 100))
+        );
+        const l = 1 - slope(d) * (1 - s);
+        const color = p.color(Math.abs(noise()), s, l);
+
+        p.pixels[PIX + 0] = p.red(color);
+        p.pixels[PIX + 1] = p.green(color);
+        p.pixels[PIX + 2] = p.blue(color);
+        p.pixels[PIX + 3] = 255;
+      }
+    }
+    p.updatePixels();
+
+    p.frameCount % 100 === 0 &&
+      console.info(`${p.ceil(progress * 100)}% | ${p.frameCount}/${FRAMES}`);
+
+    if (RENDER && p.frameCount <= FRAMES) {
+      p.save(
+        `${String(p.frameCount).padStart(String(FRAMES).length, "0")}.png`
+      );
+    }
+  };
+};
+
+new p5(sketch, "sketch");
+// document.getElementById(
+//   "description"
+// ).innerHTML = ``;
